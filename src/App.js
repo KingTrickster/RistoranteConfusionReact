93387af385da0c6f1c0a19078a355300a049019c
Alejandro Trejo <alextrejores@gmail.com>
2023-01-02 23:22:29 -0600

    Added Menu Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color='primary'>
         <div className='container'>
-          <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
+          <div className="row">
+          <NavbarBrand href='/' className="col-3">Ristorante Con Fusion</NavbarBrand>
+          </div>
         </div>
       </Navbar>
+      <Menu/>
     </div>
   );
 }
+
 
 export default App;
